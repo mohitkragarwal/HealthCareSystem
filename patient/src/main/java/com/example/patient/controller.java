577099d4c560a3f2e666b63b513a71d75ea7b830
mohit <mohitkumaragarwal8@gmail.com>
2025-12-24 16:36:09 +0530

    "created docker file"

--- patient/src/main/java/com/example/patient/controller.java
+++ patient/src/main/java/com/example/patient/controller.java
@@ -1,4 +1,13 @@
 package com.example.patient;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class controller {
+
+    @GetMapping("/getAllPatient")
+    public String getAllPatient(){
+        return "All Patient";
+    }
 }
