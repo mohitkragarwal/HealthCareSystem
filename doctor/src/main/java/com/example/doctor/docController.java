a260c08bd1d9dcc6c085e4412b31ab047c82f358
mohit <mohitkumaragarwal8@gmail.com>
2025-12-30 06:09:15 +0530

    Inter-Service Communication

--- doctor/src/main/java/com/example/doctor/docController.java
+++ doctor/src/main/java/com/example/doctor/docController.java
@@ -1,13 +1,28 @@
 package com.example.doctor;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class docController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+
     @GetMapping("/getAllDoctors")
     public String getAllDoctors(){
         return "All Doctors";
+    }
+
+    @GetMapping("/getAllPatientsFromPatientService")
+    public String getAllPatientsFromPatientService(){
+        System.out.println("Inside getAllPatientsFromPatientService");
+        String patientUrl= "http://patient-sevice:8080/getAllPatient";
+        return restTemplate.getForObject(patientUrl, String.class);
     }
 }
